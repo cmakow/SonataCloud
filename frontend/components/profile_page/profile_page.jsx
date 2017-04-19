df5ce653e6bdfa3fcde50bbabfe03ded1774d8f4
Conrad Makow <cmakow@gmail.com>
2017-04-19 17:47:32 -0400

    Added skeleton of profile page component

---
+++ frontend/components/profile_page/profile_page.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class ProfilePage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default ProfilePage;
---
+++ frontend/components/profile_page/profile_page_container.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ProfilePage from './profile_page';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => {
+
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProfilePage);
