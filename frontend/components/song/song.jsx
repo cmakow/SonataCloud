8ede72f500a029d5ca39838203bb3155b363799e
Conrad Makow <cmakow@gmail.com>
2017-04-24 15:33:15 -0400

    Added song show page + styling

--- frontend/components/song/song.jsx
+++ frontend/components/song/song.jsx
@@ -3,22 +3,51 @@ import React from 'react';
 class Song extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      song: null
+    };
+
+    this.togglePlay = this.togglePlay.bind(this);
+  }
+
+  componentDidMount() {
+    const song = this.props.fetchSong(this.props.params.id);
+    this.setState({ song });
+  }
+
+  togglePlay(e) {
+    if (this.props.song === this.props.currentSong) {
+
+    } else {
+      this.props.receiveCurrentSong(this.props.song);
+    }
   }
 
   render() {
     const song = this.props.song;
-    return (
-      <div className='songShowHeader'>
-        <button onClick={this.togglePlay} className='playButton'><i className='fa fa-play' aria-hidden='true'></i></button>
-        <div className='songShowInfo'>
-          <p>{song.artist.username}</p>
-          <h2>{song.title}</h2>
+    if(song) {
+      return (
+        <div className='songShowHeader'>
+          <div className='songShowHeaderInfo'>
+            <button onClick={this.togglePlay} className='showPlayButton'><i className='fa fa-play' aria-hidden='true'></i></button>
+            <div className='songShowInfo'>
+              <p>{song.artist.username}</p>
+              <h2>{song.title}</h2>
+            </div>
+          </div>
+          <div className='songShowCoverArt'>
+            <img src={song.cover_art} />
+          </div>
         </div>
-        <div className='songShowCoverArt'>
-          <img src={song.cover_art} />
+      );
+    } else {
+      return (
+        <div>
+          Loading...
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
--- frontend/components/song/song_container.js
+++ frontend/components/song/song_container.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Song from './song';
 import { connect } from 'react-redux';
 import { fetchSong } from '../../actions/song_index_actions';
+import { receiveCurrentSong } from '../../actions/current_song_actions';
 
-const mapStateToProps = (state, ownProps) => ({
-  song: state.songs[ownProps.params.id]
-});
+const mapStateToProps = (state, ownProps) => {
+  return ({
+    song: state.songs[ownProps.params.id]
+  });
+};
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  fetchSong: id => dispatch(fetchSong(id))
+  fetchSong: id => dispatch(fetchSong(id)),
+  receiveCurrentSong: song => dispatch(receiveCurrentSong(song))
 });
 
 export default connect(
