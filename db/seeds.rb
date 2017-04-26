72d2f9e3407253183269fb61a455c8da25506e81
Conrad Makow <cmakow@gmail.com>
2017-04-26 14:22:50 -0400

    Seeds updated to include 5 songs for demo profile, appears to work

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,12 +23,30 @@ songs = [
   'https://s3.us-east-2.amazonaws.com/sonatacloud-dev/songs/data/000/000/016/original/Garden_Song.mp3',
   'https://s3.us-east-2.amazonaws.com/sonatacloud-dev/songs/data/000/000/011/original/Fixing_a_Hole_(Colorful_Mix).mp3',
   'https://s3.us-east-2.amazonaws.com/sonatacloud-dev/songs/data/000/000/008/original/Lido_Shuffle_(rough_mix).mp3',
-  'https://s3.us-east-2.amazonaws.com/sonatacloud-dev/songs/data/000/000/009/original/No_Other_Than_The_Mother_Is_My_Song.mp3'
+  'https://s3.us-east-2.amazonaws.com/sonatacloud-dev/songs/data/000/000/009/original/No_Other_Than_The_Mother_Is_My_Song.mp3',
+  'https://s3.us-east-2.amazonaws.com/sonatacloud-dev/songs/data/000/000/006/original/Klang.mp3',
+  'https://s3.us-east-2.amazonaws.com/sonatacloud-dev/songs/data/000/000/005/Wichita+Lineman.mp3',
+  'https://s3.us-east-2.amazonaws.com/sonatacloud-dev/songs/data/000/000/007/original/Going+To+California.mp3',
+  'https://s3.us-east-2.amazonaws.com/sonatacloud-dev/songs/data/000/000/008/original/Within+You+Without+You+(Open+Door+Mix).mp3',
+  'https://s3.us-east-2.amazonaws.com/sonatacloud-dev/songs/data/000/000/009/original/Your+Gold+Dress.mp3'
+]
+
+cover_arts = [
+  'https://s3.us-east-2.amazonaws.com/sonatacloud-dev/songs/cover_arts/Klang.jpg',
+  'https://s3.us-east-2.amazonaws.com/sonatacloud-dev/songs/cover_arts/Wichita+Lineman.jpg',
+  'https://s3.us-east-2.amazonaws.com/sonatacloud-dev/songs/cover_arts/Going+to+California.jpg',
+  'https://s3.us-east-2.amazonaws.com/sonatacloud-dev/songs/cover_arts/Within+You+WIthout+You.jpg',
+  'https://s3.us-east-2.amazonaws.com/sonatacloud-dev/songs/cover_arts/Your+Gold+Dress.jpg'
 ]
 
 opened_songs = []
 songs.each_with_index do |song, i|
   opened_songs[i] = open(song)
+end
+
+opened_cover_arts = []
+cover_arts.each_with_index do |cover_art, i|
+  opened_cover_arts[i] = open(cover_art)
 end
 
 cover_art = open('https://s3.us-east-2.amazonaws.com/sonatacloud-dev/songs/cover_arts/No+Other.jpg')
@@ -37,5 +55,10 @@ Song.create([
   { title: 'Garden Song', artist_id: User.last.id, data: opened_songs[0]},
   { title: 'Fixing a Hole (Colorful Mix)', artist_id: User.last.id, data: opened_songs[1]},
   { title: 'Lido Shuffle (rough mix)', artist_id: User.last.id, data: opened_songs[2]},
-  { title: 'No Other Than The Mother Is My Song', artist_id: User.last.id, data: opened_songs[3], cover_art: cover_art}
+  { title: 'No Other Than The Mother Is My Song', artist_id: User.last.id, data: opened_songs[3], cover_art: cover_art},
+  { title: 'Klang', artist_id: User.first.id, data: opened_songs[4], cover_art: opened_cover_arts[0]},
+  { title: 'Wichita Lineman', artist_id: User.first.id, data: opened_songs[5], cover_art: opened_cover_arts[1]},
+  { title: 'Going to California', artist_id: User.first.id, data: opened_songs[6], cover_art: opened_cover_arts[2]},
+  { title: 'Within You Without You (Open Door Mix)', artist_id: User.first.id, data: opened_songs[7], cover_art: opened_cover_arts[3]},
+  { title: 'Your Gold Dress', artist_id: User.first.id, data: opened_songs[8], cover_art: opened_cover_arts[4]}
   ])
