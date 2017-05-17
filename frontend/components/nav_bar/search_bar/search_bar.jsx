7ca2d4621df6d589877ec8fc66da05931b2f21aa
Conrad Makow <cmakow@gmail.com>
2017-05-17 12:20:06 -0400

    Removed commented out code

--- frontend/components/nav_bar/search_bar/search_bar.jsx
+++ frontend/components/nav_bar/search_bar/search_bar.jsx
@@ -34,46 +34,9 @@ class SearchBar extends React.Component {
     if (this.state.input.length === 0) {
       return matches;
     }
-    //
-    // this.props.songs.forEach(song => {
-    //   let sub = song.title.slice(0, this.state.input.length);
-    //   if (sub.toLowerCase() === this.state.input.toLowerCase()) {
-    //     matches.push(song);
-    //   }
-    // });
-    //
-    // if (matches.length === 0) {
-    //   matches.push('No results');
-    // }
-    //
-    // return matches;
   }
 
   render() {
-    // let potentialMatches;
-    // if(this.props.songs) {
-    //   if(this.props.songs.length > 0) {
-    //     potentialMatches = this.matches();
-    //   }
-    // }
-    // let results = null;
-    // if(potentialMatches) {
-    //   if(Object.keys(potentialMatches).length > 0) {
-    //     results = this.matches().map((result, i) => {
-    //       return (
-            // <Link to={`/songs/${result.id}`} className='searchResultLink'>
-            //   <li key={i} className='searchResultLi'>
-            //     <img src={result.cover_art} className='searchResultIcon'/>
-            //     <div className='searchResultTitle'>{result.title}</div>
-            //   </li>
-            // </Link>
-    //       );
-    //     });
-    //   }
-    // }
-    // if(this.state.input.length === 0) {
-    //   results = null;
-    // }
     let matches;
     if(this.state.input.length > 0) {
       if(Object.keys(this.props.songs).length > 0) {
